7c3d8fb5524765a111fa71ce389eaedcc7170ef2
retr0111 <40544860+retr0111@users.noreply.github.com>
2024-03-25 20:53:47 -0700

    Update main.py

--- pokemonapi_streamlit/app/main.py
+++ pokemonapi_streamlit/app/main.py
@@ -1,1 +1,20 @@
+import streamlit as st
+from api.pokeapi import fetch_pokemon_data
 
+def display_pokemon_info(data):
+    st.title("PokeAPI Data")
+    st.write("Welcome to the PokeAPI Explorer!")
+
+    if data:
+        st.write("Available Endpoints:")
+        for endpoint, url in data.items():
+            st.write(f"- {endpoint}: {url}")
+    else:
+        st.error("Failed to fetch data from the PokeAPI. Please try again later.")
+
+def main():
+    pokemon_data = fetch_pokemon_data()
+    display_pokemon_info(pokemon_data)
+
+if __name__ == "__main__":
+    main()
